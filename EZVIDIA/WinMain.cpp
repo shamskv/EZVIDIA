1ee96e525c43ffe1a98fafa05ed9f2dd0fc7b6f6
Shams Valibhai <shams.valibhai@gmail.com>
2021-05-29 01:48:22 +0100

    clean up stuff

--- EZVIDIA/WinMain.cpp
+++ EZVIDIA/WinMain.cpp
@@ -1,3 +1,4 @@
+#include "targetver.h"
 // we need commctrl v6 for LoadIconMetric()
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #pragma comment(lib, "comctl32.lib")
--- EZVIDIA/resource.h
+++ EZVIDIA/resource.h
@@ -23,9 +23,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
