ea2ccdea72759345dbefd9bbeac53b073ceb159a
Shams Valibhai <shams.valibhai@gmail.com>
2021-06-06 01:52:55 +0100

    download executable for latest release PogU

--- EZVIDIA/src/utils/UpdaterUtils.cpp
+++ EZVIDIA/src/utils/UpdaterUtils.cpp
@@ -1,9 +1,15 @@
 #include "UpdaterUtils.hpp"
 #include "cpprest/http_client.h"
+#include <cpprest/filestream.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include<Windows.h>
+#include<debugapi.h>
 
 using namespace web;                        // Common features like URIs.
 using namespace web::http;                  // Common HTTP functionality
 using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
 
 std::wstring UpdaterUtils::getLatestVersionNumber() {
 	std::wstring owner(L"shamskv"), repo(L"EZVIDIA");
@@ -19,6 +25,18 @@ std::wstring UpdaterUtils::getLatestVersionNumber() {
 
 	if (response.status_code() == status_codes::OK) {
 		auto responseBody = response.extract_json().get();
+
+		for (auto& value : responseBody[U("assets")].as_array()) {
+			//OutputDebugStringW(value[U("name")].as_string().c_str());
+			http_client dlClient(value[U("browser_download_url")].as_string());
+			auto dlBody = dlClient.request(methods::GET).get().body();
+
+			auto outStream = fstream::open_ostream(U("dl_ezvidia.exe")).get();
+
+			dlBody.read_to_end(outStream.streambuf()).get();
+			outStream.close();
+		}
+
 		return responseBody.at(U("tag_name")).as_string();
 	}
 
